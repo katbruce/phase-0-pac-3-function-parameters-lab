f8e4ad320ae8103a40f942609fb84812ce5211a3
Kathryn <kathrynbruce7@gmail.com>
2023-02-14 15:19:58 -0700

    trial 2

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Define a Function That Uses a Parameter
 function introduction(name) {
-    console.log(`Hi, my name is ${name}.`);
+    return (`Hi, my name is ${name}.`);
 }
 
 // Define a Function That Uses Two Parameters
 function introductionWithLanguage(name, language) {
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
 
 // Define a Function That Uses an Optional Parameter
 function introductionWithLanguageOptional(name, language = "JavaScript") {
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
